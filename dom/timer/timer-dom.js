d70dc68e020ee92747d118a8e52fe3892969209c
Vani <muppuruvani13@gmail.com>
2020-05-31 22:10:03 -0500

    Thsi is dom exercise commit

--- dom/timer/timer-dom.js
+++ dom/timer/timer-dom.js
@@ -1,2 +1,15 @@
 console.log("time to start the timer!");
+let startingMinutes=10;
+let time = startingMinutes*60;
+const countDownElement=document.getElementById("timer");
+setInterval(updateCountdown,1000);
+
+function updateCountdown(){
+
+    const minutes=Math.floor(time/60);
+    let seconds=time%60;
+    seconds = seconds<10 ? 0 + seconds:seconds;
+    countDownElement.innerHTML = ` ${minutes} : ${seconds}`;
+    time--;
+}
 
